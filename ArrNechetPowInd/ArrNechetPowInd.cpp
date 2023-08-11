56f5b0f786970644cb77ec8e35a7e42b9aa5e7a9
Gridin_Andrei <andreigridin918@gmail.com>
2023-08-11 22:27:17 +0500

    Добавьте файлы проекта.

---
+++ ArrNechetPowInd/ArrNechetPowInd.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 1251 > nul");
+    int i = 0;
+    const int n = 10;
+    int arr[n];
+    while (i < n) {
+        if (i % 2 == 0) {
+            arr[i] = i;
+            cout << arr[i] << " ";
+        }
+        else {
+            arr[i] = i * i;
+            cout << arr[i] << " ";
+        }
+        i++;
+    }
+    system("pause > nul");
+    return 0;
+}
